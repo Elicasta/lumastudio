dada6fe45b0f7078b21c591f889994cc248c7635
Elicasta <130819591+Elicasta@users.noreply.github.com>
2026-09-19 17:46:51 -0400

    feat(audio): register native file dialog

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,6 +6,7 @@ use audio::AudioService;
 pub fn run() {
     tauri::Builder::default()
         .manage(AudioService::default())
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
