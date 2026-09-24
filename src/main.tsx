01cef6bc6fbb7bf14be70d9092833f2502869352
Elicasta <130819591+Elicasta@users.noreply.github.com>
2026-09-23 22:22:20 -0400

    Align LUMA desktop typography and operator surface foundations

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "./app/App";
 import { VideoOutputSurface } from "./components/VideoOutputSurface";
 import "./app/styles.css";
 import "./app/operator-ui.css";
+import "./luma-foundation.css";
 
 const surface = new URLSearchParams(window.location.search).get("surface");
 
