c01753646430527b4b30d32e4b55876d26da4270
Elicasta <130819591+Elicasta@users.noreply.github.com>
2026-09-25 14:20:54 -0400

    Link AppKit for Audio Unit custom editors

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -125,6 +125,7 @@ fn main() {
             .compile("luma_audio_unit_host");
 
         println!("cargo:rustc-link-lib=framework=Foundation");
+        println!("cargo:rustc-link-lib=framework=AppKit");
         println!("cargo:rustc-link-lib=framework=AVFoundation");
         println!("cargo:rustc-link-lib=framework=AudioToolbox");
         println!("cargo:rustc-link-lib=framework=AudioUnit");
